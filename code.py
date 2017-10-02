81faf1f2b6a2d8f92e2fc0a361e3baf19d705447
Ganesh Sivakumar <ganesh.s@dal.ca>
2017-10-02 15:55:07 +0000

    filtering dicy

---
+++ code.py
@@ -0,0 +1,12 @@
+import string
+import csv
+with open("Crime.csv", 'r') as myfile:
+	reader = csv.reader(myfile)
+	for row in reader:
+		d=dict()
+		d[row[8]]=row[7]
+		print(d)
+	for value in d:
+		d1=dict()
+		d1[value]=d.get(value,0)+1
+		print(d1)
